11db405dfac4d7b5e7a6718bce3c6c78b3848020
Logeswari Shanmugavel <logeswarishanmugavel@Logeswaris-MacBook-Pro.local>
2016-12-03 21:43:05 -0700

    pie chart for each state

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -18,6 +18,9 @@ var id_state_map = {
 var id_topo_map = {
     1: null
 };
+var statename = {
+    1: ""
+}
 var total_certified_by_state = {
     1: ""
 };
@@ -64,9 +67,6 @@ var state_link = {
     0: null
 };
 
-queue()
-    .defer(d3.json, "/h1b/alldata")
-
 d3.json("/h1b/alldata", function(error, data) {
 	d3.tsv("./static/data/us-state-names.tsv", function(error, names) {
       //  console.log(names);
@@ -79,6 +79,7 @@ d3.json("/h1b/alldata", function(error, data) {
  
         var state_id = short_name_id_map[d.employer_state];
         
+        statename[state_id] = d.employer_state;
         total_certified_by_state[state_id] = d.certified;
         total_certifiedwithdrawn_by_state[state_id] = d.certifiedwithdrawn;
         total_withdrawn_by_state[state_id] = d.withdrawn;
@@ -138,12 +139,7 @@ d3.json("/h1b/alldata", function(error, data) {
         d3.select("#canvas-svg")
         	.append("div")
           	.attr("id", "max_salary");
-        $("#max_salary").html("<h4>Maximum Salary </h4>" + "<h3 id='max_salary-value'><span class='no-data'>No Data</span></h3>");
-        
-        d3.select("#canvas-svg")
-          	.append("div")
-            .attr("id", "jobtitlewithmaxsalary");
-        $("#jobtitlewithmaxsalary").html("<h4>Job Title with the Maximum Salary</h4>" + "<h3 id='jobtitlewithmaxsalary-value'><span class='no-data'>No Data</span></h3>");
+        $("#max_salary").html("<table><tr><td><h4>Job Title with the Maximum Salary</h4>" + "<h3 id='jobtitlewithmaxsalary-value'><span class='no-data'>No Data</span></h3></td><td><h4>Maximum Salary </h4>" + "<h3 id='max_salary-value'><span class='no-data'>No Data</span></h3></td></tr></table>");
         
         d3.select("#canvas-svg")
         	.append("div")
@@ -175,6 +171,9 @@ d3.json("/h1b/alldata", function(error, data) {
     
 function map_clicked(d) {
     if (d) {
+        $('#statenamevalue').html(short_name_id_map[d.id]);
+        console.log(d);
+        drawpiechart(d);
         $("#state-select").val(d.id);
         if (max_salary_by_state[d.id]) {
             $("#max_salary-value").html(max_salary_by_state[d.id]);
@@ -212,5 +211,133 @@ function map_clicked(d) {
         g.selectAll("path").classed("active", centered && function(d) {
             return d === centered;
         });
+    }
+}
+
+function drawpiechart(d){
+    if (total_certified_by_state[d.id]) {
+    //console.log("drawing pie chart");
+    //console.log(d);
+    
+    $('#pie-chart').html("");
+    var data = [{"name":"CERTIFIED","value":total_certified_by_state[d.id]},{"name":"WITHDRAWN","value":total_withdrawn_by_state[d.id]},{"name":"CERTIFIEDWITHDRAWN","value":total_certifiedwithdrawn_by_state[d.id]},{"name":"DENIED","value":total_denied_by_state[d.id]}];
+    //console.log(data);
+
+    var r = d3.scale.category20();
+
+    var width = 550,
+    height = 350,
+    radius = Math.min(width, height) / 2;
+
+    var color = d3.scale.ordinal()
+        .range(["#0a0", "yellow", "blue", "red"]);
+
+    var arc = d3.svg.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(radius - 70);
+
+
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function(d) { return d.value; });
+
+    var svg = d3.select("#pie-chart").append("svg")
+        .attr('class','pie-chartclass')
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var g = svg.selectAll(".arc")
+        .data(pie(data))
+        .enter().append("g")
+        .attr("class", "arc");
+
+    g.append("path")
+        .attr("d", arc)
+        .style("fill", function(d) { return color(d.data.name); })
+        .on("mouseover",setCenterText)
+        .on("mouseout",resetCenterText);
+
+/*
+    g.append("text")
+        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+        .attr("dy", ".35em")
+        .text(function(d) { return d.data.value; });
+    */
+    g.append('text')
+        .attr('class', 'statenamevalue')
+        .attr('text-anchor', 'middle')
+        .style('font-size','30px')
+        .style('font-family','Helvetica')
+        .style('fill','brown');
+
+     g.select(".statenamevalue")
+            .text(id_name_map[d.id].name);
+
+    g.append('text')
+        .attr('class', 'center-txtname')
+        .attr('y', '25px')
+        .attr('text-anchor', 'middle')
+        .style('font-size','15px')
+        .style('font-family','Helvetica')
+        .style('fill','blue');
+
+    g.append('text')
+        .attr('class', 'center-txtvalue')
+        .attr('y', '50px')
+        .attr('text-anchor', 'middle')
+        .style('font-size','20px')
+        .style('font-family','Helvetica')
+        .style('fill','red');
+        ;
+    /*
+    d3.select("#pie-chart")
+            .append("div")
+            .attr("id", "center-txtvalue");
+
+    */
+
+    var piechartnames = ['CERTIFIED','WITHDRAWN','CERTIFIEDWITHDRAWN','DENIED'];
+    
+    var legends = g.select('.legend')
+                    .selectAll('g')
+                                .data(piechartnames)
+                            .enter().append('g')
+                                .attr('transform', function(d, i) {
+                                    return 'translate(' + (i * 150 + 50) + ', 10)';
+                                });
+    
+            legends.append('circle')
+                .attr('class', 'legend-icon')
+                .attr('r', 6)
+                .style('fill', function(d, i) {
+                    return color(i);
+                });
+    
+            legends.append('text')
+                .attr('dx', '1em')
+                .attr('dy', '.3em')
+                .text(function(d) {
+                    return d;
+                });
+
+    function setCenterText(d){
+        g.select(".center-txtname")
+            .text(d.data.name);
+        g.select(".center-txtvalue")
+            .text(d.data.value);
+    }
+    function resetCenterText(d){
+        g.select(".center-txtname")
+            .text("Hover to see values");
+         g.select(".center-txtvalue")
+            .text("");
+    }
+    }
+    else
+    {
+        $('#pie-chart').html("No Data");
+
     }
 }
